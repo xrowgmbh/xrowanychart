b300e2ba4e5041f461390fc80e2ed8cbe2a49d97
XROW System Account <xrow@test01.hande.c.de.siteprotect.eu>
2014-10-06 14:09:40 +0200

    Added new table attributes

---
+++ settings/content.ini.append.php
@@ -0,0 +1,12 @@
+<?php /* #?ini charset="utf-8"?
+
+[table]
+AvailableClasses[]=diagram
+CustomAttributes[]=data-diagram-type
+CustomAttributes[]=data-diagram-orientation
+ClassDescription[data-diagram-type]=Diagramm Typ
+ClassDescription[data-diagram-orientation]=Diagramm Orientierung
+CustomAttributesDefaults[data-diagram-type]=bar
+CustomAttributesDefaults[data-diagram-orientation]=vertical  
+
+*/ ?>
---
+++ settings/ezoe_attributes.ini.append.php
@@ -0,0 +1,34 @@
+<?php /* #?ini charset="utf-8"?
+
+[CustomAttribute_table_data-diagram-orientation]
+Name=Diagramm Orientierung 
+Title=Defines orientation of diagramm
+Default=vertical
+Type=select
+Disabled=false
+Required=false
+Selection[]
+Selection[vertical]=Vertical
+Selection[horizontal]=Horizontal
+
+[CustomAttribute_table_data-diagram-type]
+Name=Diagramm Typ
+Title=Defines type of diagramm
+Default=bar
+Type=select
+Disabled=false
+Required=false
+Selection[] 
+Selection[bar]=Bar
+Selection[3d-bar]=3D-Bar
+Selection[line]=Line
+Selection[step-line]=Step-Line  
+Selection[spline]=Spline
+Selection[area]=Area
+Selection[spline-Area]=Spline-Area  
+Selection[step-area]=Step-Area
+Selection[marker]=Marker
+Selection[pie]=Pie
+Selection[doughnut]=Doughnut
+
+*/ ?>
